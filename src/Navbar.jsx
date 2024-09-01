d4adc07854e9cca39e82147331a6b64e8a9e9dd4
Naman Dadhich <namandadhich15592@gmail.com>
2024-09-01 23:03:31 +0530

    Initial commit

---
+++ src/Navbar.jsx
@@ -0,0 +1,9 @@
+import logo from './assets/logo and avatars/logo.jpg';
+
+export default function Navbar(){
+    return(
+        <div>
+            <img src={logo} alt='logo'></img>
+        </div>
+    )
+}
