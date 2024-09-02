ae2f3b40d53c4024db0eaadc0900867128118e83
Naman Dadhich <namandadhich15592@gmail.com>
2024-09-02 15:51:57 +0530

    logo is added

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,9 +1,11 @@
 import logo from './assets/logo and avatars/logo.jpg';
+import './App.css';
+
 
 export default function Navbar(){
     return(
         <div>
-            <img src={logo} alt='logo'></img>
+            <img src={logo} alt='logo' ></img>
         </div>
     )
 }
