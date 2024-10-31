5fda66eb87e94c7a0b672a4aecbc02a0cbfcacd8
Khushipawar37 <khushipawar987@gmail.com>
2024-10-31 22:15:17 +0530

    added data for tech card

---
+++ src/Career/flowData.js
@@ -0,0 +1,26 @@
+const flowData = {
+    "Software Engineer": {
+      title: "Career Path",
+      path: [
+        {
+          level: "Stream Selection",
+          options: ["PCM (Physics, Chemistry, Mathematics)"]
+        },
+        {
+          level: "Entrance Exams",
+          options: ["JEE Main", "BITSAT", "VITEEE", "UPSEE", "COMEDK UGET"]
+        },
+        {
+          level: "Undergraduate Options",
+          options: ["B.Tech in Computer Science", "B.Tech in Information Technology", "B.Sc in Computer Science", "BCA (Bachelor of Computer Applications)"]
+        },
+        {
+          level: "Postgraduate Options",
+          options: ["M.Tech in Computer Science", "MCA (Master of Computer Applications)", "MBA in IT", "M.Sc in Computer Science", "Ph.D. in Computer Science"]
+        },
+      ]
+    },
+  };
+  
+  export default flowData;
+  
