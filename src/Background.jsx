906516a912b72fc7739ef5feb0ee47429fffd79d
Naman Dadhich <namandadhich15592@gmail.com>
2025-01-07 10:24:06 +0530

    bg2

--- src/Background.jsx
+++ src/Background.jsx
@@ -3,7 +3,7 @@ import './Background.css';
 
 const Background = () => {
   return (
-    <div className="background">
+    <div className="fixed top-0 left-0 w-full h-[100vh] bg-[#1b1b1b] z-[-1]">
       <div className="checkered-pattern"></div>
     </div>
   );
